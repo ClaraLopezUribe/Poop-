2532f5687e156670883e1115dc193a723d5a37b6
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-08-10 14:59:05 -0400

    Initial Commit
    Structures the app functions in pseudocode.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,76 @@
-//Call Hello World
-function helloWorld() {
-    alert ("hello World");
+// ----- CONTROLLER function -----//
+
+    
+function getString () {
+
+    // Gets the user input string from the DOM and assigns it to a variable
+    let userString = document.getElementById("userString").value;
+
+    // Converts userString toUpperCase()
+    userString = userString.toUpperCase()
+
+    // Calls the Logic function and assigns the return value to a variable
+    let reversedString = reverse(userString);
+
+    // Calls the Display function
+    display();
+
+
+
+
 }
+
+   
+    
+
+
+// ------ LOGIC function -----//
+
+function reverse(userString) {
+
+    reversedString = []
+
+    // Checks for length of the string and 
+    // Decrements through the indices
+    
+   for (i = userString.length - 1; i >= 0; i--) {
+    
+     // Cleans up input (Take out spaces, reduce to lower case letters, ignore special characters); RegExp
+        // if i is any charachter other than an upper or lower case letter, ignore
+        
+     
+    
+     // Else, 
+     if (i )
+
+
+    // Stores in new array
+    reversedString += userString[index];
+
+   }
+        
+   if reversedString == userString {
+
+
+   }
+    // Test whether the user input string is the same as the reversed string. Use .includes() method
+
+
+}
+
+function display() {
+
+
+}   
+// Display message confirming it is a palindrome or not
+
+
+
+    
+
+
+
+
+// DISPLAY functtion
+
+
