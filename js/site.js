0c0686962af4a596ec1fae7d5c40cf51f32bfcef
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-08-13 15:37:36 -0400

    Refactor js code

--- js/site.js
+++ js/site.js
@@ -7,31 +7,34 @@ function getUserString() {
     // Gets the user input from the DOM and assigns it to a variable
     let userString = document.getElementById("userString").value;
 
-    // Cleans up the user input (Ignores special characters, removes spaces, and converts all characters to lowercase) to facilitate comparison
-    let cleanString = userString.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    // Cleans up the user input (Ignores special characters, removes spaces, and converts all characters to lowercase) to facilitate comparison using two js methods
+    let cleanString = userString.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
     // Calls the Logic function and assigns the return value to a variable
     let reversedString = reverse(cleanString);
 
-    if (reversedString == cleanString) {
 
-    // Calls the Display function
-     display (userString);
+    if (userString === "") {
+
+       let message = `Yeah, ok...it's technically a palindrome, but why don't you "push" a little harder by using a few more characters`;
+
+    } else if (reversedString === cleanString) {
+
+        message = `You came out smelling like roses!!!<br><br>${userString} is a palindrome!`;
 
     } else {
 
-        document.getElementById("result").innerHTML = `That stinks...<br><br>"${userString}" is not a palindrome! Try again!`;
-        document.getElementById("alert").classList.remove("invisible");
-
+        message = `That stinks...<br><br>"${userString}" is not a palindrome! Try again!`;
 }
 
    
-    
 }
 
 // ------ LOGIC -----//
 
 function reverse(cleanString) {
+
+   /* This was my original solution:
 
     let reversedArray = [];
 
@@ -39,12 +42,13 @@ function reverse(cleanString) {
    for (i = cleanString.length - 1; i >= 0; i--) {
     
     // Adds each index value to the new array
-     reversedArray += cleanString[i];
-
+        reversedArray.push(cleanString[i]);
    }
         
-    return reversedArray;
+    return reversedArray.join(""); */
 
+    //I later learned that by "method chaining", we can achieve the same result in a more effficient way
+    return cleanString.split("").reverse().join("");
 }
 
 // ----- DISPLAY -----//
