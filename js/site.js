0ef8b589c60206ebc3ad4d6a15b8d6a5cb7e6384
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-08-13 20:06:06 -0400

    Refactor js code and update corresponding html

--- js/site.js
+++ js/site.js
@@ -1,8 +1,6 @@
 // ----- CONTROL -----//
    
-function getUserString() {
-
-    document.getElementById("alert").classList.add("invisible");
+function prepUserString() {
 
     // Gets the user input from the DOM and assigns it to a variable
     let userString = document.getElementById("userString").value;
@@ -10,67 +8,41 @@ function getUserString() {
     // Cleans up the user input (Ignores special characters, removes spaces, and converts all characters to lowercase) to facilitate comparison using two js methods
     let cleanString = userString.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
-    // Calls the Logic function and assigns the return value to a variable
-    let reversedString = reverse(cleanString);
+    //Clears input field
+    document.getElementById("userString").value = "";
+
+    checkPalindrome(cleanString, userString);
+}
 
 
-    if (userString === "") {
+// ------ LOGIC -----//
 
-       let message = `Yeah, ok...it's technically a palindrome, but why don't you "push" a little harder by using a few more characters`;
+function checkPalindrome(cleanString, userString) {
+   
+    let reversedString = cleanString.split("").reverse().join("");
 
-    } else if (reversedString === cleanString) {
+    let message = "";
+
+    if (userString == "" || userString.length == 1) {
+
+        message = `Yeah, ok...this is technically a palindrome, but why don't you "push" a little harder by using a few more characters`;
+
+     } else if (reversedString == cleanString) {
 
         message = `You came out smelling like roses!!!<br><br>${userString} is a palindrome!`;
 
     } else {
 
-        message = `That stinks...<br><br>"${userString}" is not a palindrome! Try again!`;
+         message = `Well, that stinks...<br><br>"${userString}" is not a palindrome! Try again!`;
 }
 
-   
-}
-
-// ------ LOGIC -----//
-
-function reverse(cleanString) {
-
-   /* This was my original solution:
-
-    let reversedArray = [];
-
-    // Checks for length of the string to decrement starting from last index
-   for (i = cleanString.length - 1; i >= 0; i--) {
-    
-    // Adds each index value to the new array
-        reversedArray.push(cleanString[i]);
-   }
-        
-    return reversedArray.join(""); */
-
-    //I later learned that by "method chaining", we can achieve the same result in a more effficient way
-    return cleanString.split("").reverse().join("");
+    displayResults(message);
 }
 
 // ----- DISPLAY -----//
 
-function display(userString) {
+function displayResults(message) {
 
-
-
-
-document.getElementById("result").innerHTML = `You came out smelling like roses!!!<br><br>${userString} is a palindrome!`;
+    document.getElementById("result").innerHTML = message;
 document.getElementById("alert").classList.remove("invisible");
-
-}   
-
-
-
-
-    
-
-
-
-
-
-
-
+}
